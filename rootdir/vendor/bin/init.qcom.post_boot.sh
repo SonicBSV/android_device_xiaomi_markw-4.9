c5ea9754bc62efe26e0df68b49031eb2ee630319
SonicBSV <sonicbsv@gmail.com>
2018-08-16 17:25:17 +0300

    change scaling cpu to 2016000 max freq

--- rootdir/vendor/bin/init.qcom.post_boot.sh
+++ rootdir/vendor/bin/init.qcom.post_boot.sh
@@ -251,7 +251,7 @@ case "$target" in
 
                 # default freq min/max, gov and i/o sched
                 echo 652800 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-                echo 1958400 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+                echo 2016000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
                 echo \"interactive\" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
                 echo cfq > /sys/block/mmcblk0/queue/scheduler
 
