f0681bd66809abab1210b7d59f715e0987c63889
SonicBSV <sonicbsv@gmail.com>
2019-05-06 14:29:23 +0300

    Evolution X Pie

---
+++ aosp_markw.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#               2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Evolution X stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_DENSITY := xxhdpi
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit from markw device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Xiaomi
+PRODUCT_NAME := aosp_markw
+BOARD_VENDOR := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_DEVICE := markw
+PRODUCT_MODEL := Redmi 4
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V10.2.1.0.MBEMIXM release-keys" \
+    TARGET_DEVICE="markw" \
+    DEVICE_MAINTAINERS="SonicBSV"
+    
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V10.2.1.0.MBEMIXM:user/release-keys
