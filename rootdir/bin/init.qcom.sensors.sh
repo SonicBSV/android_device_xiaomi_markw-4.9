18af3a1cd29d0fb32e4330ad118ac2baca9a80d6
SonicBSV <sonicbsv@gmail.com>
2019-07-11 22:53:25 +0300

    rootdir: up init files from CAF LA.UM.7.6.r1-05500-89xx.0

--- rootdir/bin/init.qcom.sensors.sh
+++ rootdir/bin/init.qcom.sensors.sh
@@ -32,8 +32,8 @@
 start_sensors()
 {
     sscrpcd_status=`getprop init.svc.vendor.sensors`
-    chmod -h 664 /mnt/vendor/persist/sensors/sensors_settings
-    chown -h -R system.system /mnt/vendor/persist/sensors
+    chmod -h 664 /persist/sensors/sensors_settings
+    chown -h -R system.system /persist/sensors
     start vendor.sensors.qti
 
     # Only for SLPI
