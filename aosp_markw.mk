d5d522a27e2c0ae4dd2a8e5f1f6a87f869557bc4
SonicBSV <sonicbsv@gmail.com>
2019-10-21 21:46:17 +0300

    fix cts pass

--- aosp_markw.mk
+++ aosp_markw.mk
@@ -39,9 +39,8 @@ PRODUCT_MODEL := Redmi 4 Prime
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="walleye-user 9 PQ3A.190505.001 5373320 release-keys" \
-    TARGET_DEVICE="markw" \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.021 4565141 release-keys" \
     DEVICE_MAINTAINERS="SonicBSV"
-    
+
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/walleye/walleye:9/PQ3A.190505.001/5373320:user/release-keys
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.021/4565141:user/release-keys
