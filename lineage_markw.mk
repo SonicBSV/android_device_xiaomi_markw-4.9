a75c913fba66df7898d7baa930ec614fa50d61a5
SonicBSV <sonicbsv@gmail.com>
2020-10-29 18:38:22 +0300

    up from 29/10/2020

--- lineage_markw.mk
+++ lineage_markw.mk
@@ -5,6 +5,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit from markw device
 IS_PHONE := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
