9ce3f0f66e12fb6f033bc8f48d2e6471a1b6a99e
SonicBSV <sonicbsv@gmail.com>
2022-10-27 16:36:39 +0300

    markw: custom usb service for markw from daisy

--- device.mk
+++ device.mk
@@ -424,7 +424,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.markw
 
 # vndservicemanager
 PRODUCT_PACKAGES += \
