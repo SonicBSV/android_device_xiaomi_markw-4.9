eeb702de14d725a9766601d999cc6f23ad66e113
rianza <116883839+rianza@users.noreply.github.com>
2022-11-04 22:10:21 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_markw.mk
+    $(LOCAL_DIR)/derp_markw.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_markw-user \
-    lineage_markw-userdebug \
-    lineage_markw-eng
+    derp_markw-user \
+    derp_markw-userdebug \
+    derp_markw-eng
