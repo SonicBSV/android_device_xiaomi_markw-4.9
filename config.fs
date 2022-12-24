345e5a76666349adca5a952d4ec1c198b627689e
SonicBSV <sonicbsv@gmail.com>
2022-12-24 16:49:34 +0300

    unused camera daemon entries

--- config.fs
+++ config.fs
@@ -61,18 +61,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
-[vendor/bin/mm-qcamera-daemon]
-mode: 0700
-user: AID_CAMERA
-group: AID_SHELL
-caps: SYS_NICE
-
-[system/vendor/bin/mm-qcamera-daemon]
-mode: 0700
-user: AID_CAMERA
-group: AID_SHELL
-caps: SYS_NICE
-
 [vendor/bin/loc_launcher]
 mode: 0755
 user:  AID_GPS
