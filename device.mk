00f9b593c12047bc306bb8ee5d644893b6d5280e
derveror <58082462+derveror@users.noreply.github.com>
2022-12-30 22:50:15 +0300

    ekixir

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ TARGET_BOARD_SUFFIX := _64
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-elixir
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
