c3105eca2b3932fa0db3ba156d58f203ec26c06b
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2023-01-23 12:04:28 +0700

    Create Android.mk

---
+++ remove_packages/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := Camera2 Drive Maps Photos MaestroPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true 
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
