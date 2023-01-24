9192fdc61bc6e9b9d19ab12e26ca937c09d78eb5
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2023-01-24 17:21:38 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -461,3 +461,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# Remove Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
