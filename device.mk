63f9aede72bf06ffe0e75447f29ed95f8cb3c2a4
SonicBSV <sonicbsv@gmail.com>
2023-01-26 22:02:21 +0300

    markw: add display config file
     * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
     * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Signed-off-by: orges <me@orgesified.ml>
    Co-authored-by: orges <me@orgesified.ml>
    Change-Id: Ieb69a8c8098b7ac236338b3619d2c6cf963b82fc

--- device.mk
+++ device.mk
@@ -218,6 +218,10 @@ PRODUCT_PACKAGES += \
     libEGL_adreno_libEGL_adreno_symlink64 \
     libGLESv2_adreno_libGLESv2_adreno_symlink64
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     DeviceDoze \
