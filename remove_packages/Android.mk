1034636898be24f784b6454a4962a29bc3abb565
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2023-01-27 21:00:28 +0700

    markw PixysOS build

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -6,6 +6,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := /dev/null
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_OVERRIDES_PACKAGES := arcore BetterBugStub Chrome GoogleCamera GoogleFeedback GooglePrintRecommendationService GoogleTTS Papers SafetyHubPrebuilt talkback Velvet
+LOCAL_OVERRIDES_PACKAGES := arcore BetterBugStub Chrome GoogleCamera GoogleFeedback GooglePrintRecommendationService GoogleTTS Papers SafetyHubPrebuilt talkback Velvet Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 include $(BUILD_PREBUILT)
