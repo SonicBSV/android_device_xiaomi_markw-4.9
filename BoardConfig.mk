40d7b7203492562a311157c84c5148b3437adc5f
SonicBSV <sonicbsv@gmail.com>
2023-03-26 13:38:33 +0300

    markw: set SELINUX_IGNORE_NEVERALLOWS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,7 @@ VENDOR_SECURITY_PATCH := 2022-09-05
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
