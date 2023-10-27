4a20805fac93226689dc3fed9d8ee5c6ae4f4506
SonicBSV <sonicbsv@gmail.com>
2023-10-28 02:56:32 +0300

    Revert "markw: add vendor.lineage.health"
    This reverts commit 9d190c03aef6b6bc6dae1ddd5538d4fe66417649.

--- device.mk
+++ device.mk
@@ -311,10 +311,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi_markw
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service.xiaomi_markw
