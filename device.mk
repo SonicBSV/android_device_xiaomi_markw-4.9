53c2e4bc0a7d4797485479a4fdc447ccbd5cf9bf
SonicBSV <sonicbsv@gmail.com>
2023-10-28 03:45:43 +0300

    markw: Import audio_io_policy.conf from CLO
    D audio_hw_utils: audio_extn_utils_update_streams_cfg_lists: failed to open io config file(/vendor/etc/audio_io_policy.conf), trying older config file
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I993731771905d856daba96a1dfb733f8e79cd40b

--- device.mk
+++ device.mk
@@ -141,7 +141,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/configs/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml \
+    $(LOCAL_PATH)/configs/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf
 
 # Bluetooth
 PRODUCT_PACKAGES += \
