2e127b3ecb29be308b84b9cfbcd989ef19164870
SonicBSV <sonicbsv@gmail.com>
2023-12-17 06:01:50 +0300

    markw: switch to retrofit dynamic partitions
    Co-authored-by: @me-cafebabe

--- device.mk
+++ device.mk
@@ -20,6 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 TARGET_BOARD_PLATFORM := msm8953
 TARGET_BOARD_SUFFIX := _64
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
