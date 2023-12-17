d5bd665946ce49d1a125932da2588fe39702e1a9
SonicBSV <sonicbsv@gmail.com>
2023-12-17 05:52:11 +0300

    markw: Flash dummy super image if needed
    * Needed because stock does not use dynamic partitions so
      there's no existing metadata that could be used
    
    Co-authored-by: @Linux4

---
+++ releasetools.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2009 The Android Open Source Project
+# Copyright (c) 2011, The Linux Foundation. All rights reserved.
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import common
+import re
+
+def FullOTA_InstallBegin(info):
+  AddImage(info, "RADIO", "super_dummy.img", "/tmp/super_dummy.img");
+  flash_script = open("device/xiaomi/markw/partitions/flash_super_dummy.sh", 'r').read()
+  common.ZipWriteStr(info.output_zip, "install/bin/flash_super_dummy.sh", flash_script);
+  info.script.AppendExtra('package_extract_file("install/bin/flash_super_dummy.sh", "/tmp/flash_super_dummy.sh");')
+  info.script.AppendExtra('run_program("/sbin/sh", "/tmp/flash_super_dummy.sh");')
+  return
+
+def AddImage(info, dir, basename, dest):
+  path = dir + "/" + basename
+  if path not in info.input_zip.namelist():
+    return
+
+  data = info.input_zip.read(path)
+  common.ZipWriteStr(info.output_zip, basename, data)
+  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
