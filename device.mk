b8134681f77d7dbc1e8d4c33e940b2fb322f4bc9
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-12-27 01:10:45 +0100

    markw: Keep building health HIDL interface lib in vendor
    * GNSS HAL needs it

--- device.mk
+++ device.mk
@@ -286,6 +286,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
+    android.hardware.health@2.1.vendor \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
