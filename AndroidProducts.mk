31aebda1b67c32ce8af742c5fc2b15475818f93d
SonicBSV <sonicbsv@gmail.com>
2024-07-25 20:31:50 +0300

    markw: update lunch choice

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,6 +17,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_markw.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_markw-ap1a-user \
-    lineage_markw-ap1a-userdebug \
-    lineage_markw-ap1a-eng
+    lineage_markw-ap2a-user \
+    lineage_markw-ap2a-userdebug \
+    lineage_markw-ap2a-eng
