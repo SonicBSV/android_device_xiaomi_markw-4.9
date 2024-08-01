f2986b4e2ee9b34d7afd0cf8e6bde08208a7e4ab
SonicBSV <sonicbsv@gmail.com>
2024-08-01 06:52:52 +0300

    Revert "markw: Use the common device compatibility matrix from QCOM"
    This reverts commit 46eaa58a60b999618529ca484f359688a440fadc.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,7 +140,7 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE   := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # Init
