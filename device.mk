43ea64df9d2d6576ae841b2b86b80b57bb568979
SonicBSV <sonicbsv@gmail.com>
2024-08-03 12:19:35 +0300

    Revert "markw: device: Sets Android Go recommended default product options"
    This reverts commit 554d570554a85eef3f0e102526862509228ed371.

--- device.mk
+++ device.mk
@@ -230,13 +230,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
-USE_DEX2OAT_DEBUG := false
-
-# Disable Scudo outside of eng builds to save RAM.
-PRODUCT_DISABLE_SCUDO := true
-
-# Dedupe VNDK libraries with identical core variants.
-TARGET_VNDK_USE_CORE_VARIANT := true    
+USE_DEX2OAT_DEBUG := false    
 
 # DRM
 PRODUCT_PACKAGES += \
