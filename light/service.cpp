18e9cd24404f9e33e3ae1ff7d930d81d10e10b18
SonicBSV <sonicbsv@ya.ru>
2024-08-21 02:43:09 +0300

    markw: aidl: light: mido => markw

--- light/service.cpp
+++ light/service.cpp
@@ -6,7 +6,7 @@
 
 #include "Lights.h"
 
-#define LOG_TAG "android.hardware.light-service.xiaomi_mido"
+#define LOG_TAG "android.hardware.light-service.xiaomi_markw"
 
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
