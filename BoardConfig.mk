f7db7380db26bf58456a4af99518022a7fb02961
SonicBSV <sonicbsv@ya.ru>
2024-08-28 18:08:47 +0300

    markw: sepolicy: update & fixes neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,7 +171,6 @@ VENDOR_SECURITY_PATCH := 2023-06-05
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
