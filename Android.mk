c9ccbb7d350829709f59f12c1cf9cc3deabe1876
SonicBSV <sonicbsv@ya.ru>
2024-08-28 17:54:06 +0300

    markw: add symlink for msadp

--- Android.mk
+++ Android.mk
@@ -20,6 +20,11 @@ include $(call all-subdir-makefiles)
 
 include $(CLEAR_VARS)
 
+# Create symbolic links for msadp
+$(shell  mkdir -p $(TARGET_OUT_VENDOR)/firmware; \
+	ln -sf /dev/block/bootdevice/by-name/msadp \
+	$(TARGET_OUT_VENDOR)/firmware/msadp)
+
 # A/B builds require us to create the mount points at compile time.
 # Just creating it for all cases since it does not hurt.
 FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
