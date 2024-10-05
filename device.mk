2b49b4b99c0f1444fa139bcd593493b11161f682
SonicBSV <sonicbsv@ya.ru>
2024-10-05 20:23:35 +0300

    markw: Xtended-ify!

--- device.mk
+++ device.mk
@@ -452,10 +452,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.msm8953:64
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
