2f911ac6e18ee084507403dd17b1cab8be5af738
SonicBSV <sonicbsv@ya.ru>
2024-10-05 20:20:43 +0300

    optimize overlays for MSM Xtended XR

--- device.mk
+++ device.mk
@@ -28,7 +28,6 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
