b5b577dff84410bf1736d73a6c326127dfd26a81
SonicBSV <sonicbsv@ya.ru>
2024-11-30 02:02:35 +0300

    markw: patch for non A/B devices

--- lineage_markw.mk
+++ lineage_markw.mk
@@ -17,6 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit from markw device
 $(call inherit-product, device/xiaomi/markw/device.mk)
