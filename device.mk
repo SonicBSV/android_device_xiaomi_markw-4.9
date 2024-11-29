a4e3ba466bb630db7b182cbaebb7e69bb0a3f76d
SonicBSV <sonicbsv@ya.ru>
2024-11-30 01:56:55 +0300

    Revert "markw: set TARGET_VNDK_USE_CORE_VARIANT := true"
    This reverts commit 5b411d760047ba64be68d1156edef6291e31a902.

--- device.mk
+++ device.mk
@@ -202,9 +202,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.use_buffer_age=true
 
-# Dedupe VNDK libraries with identical core variants.
-TARGET_VNDK_USE_CORE_VARIANT := true
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
