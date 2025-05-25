ed315f80e1b035275fdf9895cd1d6bfcb59fe804
SonicBSV <sonicbsv@ya.ru>
2025-05-26 02:13:21 +0300

    markw: drop system_ext property

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,6 @@ BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 # Device Properties
 TARGET_ODM_PROP := $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP := $(DEVICE_PATH)/product.prop
-TARGET_SYSTEM_EXT_PROP := $(DEVICE_PATH)/system_ext.prop
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP := $(DEVICE_PATH)/vendor.prop
 
