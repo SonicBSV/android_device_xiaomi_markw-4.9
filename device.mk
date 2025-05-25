ab166d560c53c2e394a82fbe56aa36b4a31d07b2
SonicBSV <sonicbsv@ya.ru>
2025-05-26 02:43:22 +0300

    markw: Switch to QTI Memtrack AIDL HAL
    Co-authored-by: @me-cafebabe

--- device.mk
+++ device.mk
@@ -197,15 +197,13 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    vendor.qti.hardware.memtrack-service \
     gralloc.msm8953 \
     hwcomposer.msm8953 \
     libgralloc.system.qti \
     libqdMetaData.system \
     libtinyxml \
     libvulkan \
-    memtrack.msm8953 \
     vendor.display.config@1.0.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor
