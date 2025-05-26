965f47b66745649210ccfb60f164cab0ffaecf89
SonicBSV <sonicbsv@ya.ru>
2025-05-26 04:05:34 +0300

    Revert "mido: Makefile: Inherit several Android Go configurations"
    This reverts commit 715cdf88ad5b16ea69acd509d62d617f47a1f1d4.

--- device.mk
+++ device.mk
@@ -318,10 +318,6 @@ PRODUCT_PACKAGES += \
     libimscamera_jni_libimscamera_jni_symlink64 \
     libimsmedia_jni_libimsmedia_jni_symlink64
 
-# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5x06_ts.kl \
