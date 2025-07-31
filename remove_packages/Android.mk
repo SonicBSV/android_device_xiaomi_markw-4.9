2cfc45dc61520249a4ee42309896c6d4b784000e
SonicBSV <sonicbsv@ya.ru>
2025-08-01 00:45:03 +0300

    mido: Opt out DeviceDiagnostics package

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := NfcNci SecureElement DeviceAsWebcam
+LOCAL_OVERRIDES_PACKAGES := NfcNci SecureElement DeviceAsWebcam DeviceDiagnostics
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
